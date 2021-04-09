6819e167b5b71cbd7124c8c0947135e10cb49916
lzx4229303 <1063375091@qq.com>
2021-04-09 14:00:52 +0800

    最长公共前缀

---
+++ src/_0014longestCommonPrefix.java
@@ -0,0 +1,39 @@
+/**
+ * @author: lzx@4229303
+ * @create: 04-09
+ * 最长公共前缀
+ */
+
+
+/**
+ * 纵向比较
+ *
+ * f-l-o-w-e-r      |||
+ * | |              |||
+ * f-l-o-w          |||
+ * | |              |||
+ * f-l-i-g-h-t      |||
+ * | |            \\|||//
+ * f-l-a-g         \\ //
+ *
+ */
+public class _0014longestCommonPrefix {
+    public String longestCommonPrefix(String[] strs){
+        if(strs==null||strs.length==0){return "";}
+
+        int len=strs[0].length();
+        int count=strs.length;
+
+        for(int n=0;n<len;n++){
+            char c=strs[0].charAt(n);
+            for(int j=1;j<count;j++){
+                if(n==strs[j].length()||c!=strs[j].charAt(n)){
+                    return strs[0].substring(0,n);
+                }
+            }
+        }
+
+
+      return strs[0];
+    }
+}
