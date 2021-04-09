fa8c33a08cc2770a7ab0682a2a30c4b25b514226
lzx4229303 <1063375091@qq.com>
2021-04-09 14:36:18 +0800

    最长公共前缀

--- src/_0014longestCommonPrefix.java
+++ src/_0014longestCommonPrefix.java
@@ -7,7 +7,7 @@
 
 /**
  * 纵向比较
- *
+ * <p>
  * f-l-o-w-e-r      |||
  * | |              |||
  * f-l-o-w          |||
@@ -15,25 +15,30 @@
  * f-l-i-g-h-t      |||
  * | |            \\|||//
  * f-l-a-g         \\ //
- *
  */
 public class _0014longestCommonPrefix {
-    public String longestCommonPrefix(String[] strs){
-        if(strs==null||strs.length==0){return "";}
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
 
-        int len=strs[0].length();
-        int count=strs.length;
+        int len = strs[0].length();
+        int count = strs.length;
 
-        for(int n=0;n<len;n++){
-            char c=strs[0].charAt(n);
-            for(int j=1;j<count;j++){
-                if(n==strs[j].length()||c!=strs[j].charAt(n)){
-                    return strs[0].substring(0,n);
+        for (int n = 0; n < len; n++) {
+            char c = strs[0].charAt(n);
+            for (int j = 1; j < count; j++) {
+                if (n == strs[j].length() || c != strs[j].charAt(n)) {
+                    return strs[0].substring(0, n);
+
                 }
             }
         }
 
 
-      return strs[0];
+        return strs[0];
     }
+
+
+
 }
