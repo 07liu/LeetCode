5b07ae2d5d4ed082fe8a060b348024c887a5435b
lzx4229303 <1063375091@qq.com>
2021-04-12 10:23:51 +0800

    两数之和

--- src/_0001twoSum.java
+++ src/_0001twoSum.java
@@ -13,18 +13,19 @@ public class _0001twoSum {
     /**
      * 哈希表法
      * 遍历一次 高效
+     *
      * @param nums
      * @param target
      * @return
      */
-    public int[] twoSum(int[] nums,int target){
+    public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            if(hashMap.containsKey(target-nums[i])){
-                return new int[]{hashMap.get(target-nums[i]),i};
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
             }
-            hashMap.put(nums[i],i);
+            hashMap.put(nums[i], i);
         }
 
         return new int[0];
@@ -34,12 +35,12 @@ public class _0001twoSum {
      * 暴力破解
      * 循环了！num.length次 效率不高 速度快
      */
-    public int[] twoSum_2(int[] nums,int target){
+    public int[] twoSum_2(int[] nums, int target) {
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
