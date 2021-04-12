ec7eb3db10f216f73a6bec814893b29f5917c025
lzx4229303 <1063375091@qq.com>
2021-04-12 10:21:53 +0800

    加入暴力破解的方法

--- src/_0001twoSum.java
+++ src/_0001twoSum.java
@@ -9,6 +9,14 @@ import java.util.Map;
 
 
 public class _0001twoSum {
+
+    /**
+     * 哈希表法
+     * 遍历一次 高效
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums,int target){
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
@@ -19,6 +27,22 @@ public class _0001twoSum {
             hashMap.put(nums[i],i);
         }
 
+        return new int[0];
+    }
+
+    /**
+     * 暴力破解
+     * 循环了！num.length次 效率不高 速度快
+     */
+    public int[] twoSum_2(int[] nums,int target){
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
         return new int[0];
     }
 }
